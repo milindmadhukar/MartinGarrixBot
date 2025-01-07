9b9de54ae8d3100c6253b17df21a07581967a2de
Milind <m.milind15@gmail.com>
2025-01-07 19:03:13 +0530

    logging

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/golang-migrate/migrate/v4 v4.18.1
 	github.com/jackc/pgx/v5 v5.7.2
 	github.com/pelletier/go-toml/v2 v2.2.3
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
 
 require (
--- mgbot/handlers/handlers.go
+++ mgbot/handlers/handlers.go
@@ -1,13 +1,31 @@
 package handlers
 
 import (
+	"context"
+
 	"github.com/disgoorg/disgo/bot"
 	"github.com/disgoorg/disgo/events"
+	"github.com/jackc/pgx/v5/pgtype"
+	db "github.com/milindmadhukar/MartinGarrixBot/db/sqlc"
 	"github.com/milindmadhukar/MartinGarrixBot/mgbot"
 )
 
 func MessageHandler(b *mgbot.MartinGarrixBot) bot.EventListener {
 	return bot.NewListenerFunc(func(e *events.MessageCreate) {
-		// TODO: handle message
+		// TODO: Check for blocking
+		// TODO : Fetch user
+
+		b.Queries.MessageSent(context.Background(), db.MessageSentParams{
+			MessageID: int64(e.MessageID),
+			ChannelID: int64(e.ChannelID),
+			AuthorID:  int64(e.Message.Author.ID),
+			Content:   e.Message.Content,
+			// FIX: Placeholder for now
+			TotalXp: pgtype.Int4{
+				Int32: 1000,
+				Valid: true,
+			},
+			LastXpAdded: pgtype.Timestamp{},
+		})
 	})
 }
