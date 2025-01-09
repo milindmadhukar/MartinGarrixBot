f9244a43228db89e267111c43ccf4334b5323120
Milind <m.milind15@gmail.com>
2025-01-09 12:42:43 +0530

    avatar

---
+++ mgbot/commands/avatar.go
@@ -0,0 +1,38 @@
+package commands
+
+import (
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/handler"
+	"github.com/milindmadhukar/MartinGarrixBot/utils"
+)
+
+var avatar = discord.SlashCommandCreate{
+	Name:        "avatar",
+	Description: "Get the avatar of a member.",
+	Options: []discord.ApplicationCommandOption{
+		discord.ApplicationCommandOptionUser{
+			Name:        "user",
+			Description: "The user to get the avatar of.",
+			Required:    false,
+		},
+	},
+}
+
+func AvatarHandler(e *handler.CommandEvent) error {
+	user := e.SlashCommandInteractionData().User("user")
+	if user.ID == 0 {
+		user = e.Member().User
+	}
+	avatarURL := user.AvatarURL(discord.WithFormat(discord.FileFormatJPEG), discord.WithSize(1024))
+
+	eb := discord.NewEmbedBuilder().
+		SetTitle("Avatar").
+		SetColor(utils.ColorSuccess).
+		SetImage(*avatarURL)
+
+	return e.Respond(
+		discord.InteractionResponseTypeCreateMessage, discord.NewMessageCreateBuilder().
+			SetEmbeds(eb.Build()).
+			Build(),
+	)
+}
--- mgbot/commands/commands.go
+++ mgbot/commands/commands.go
@@ -1,8 +1,11 @@
 package commands
 
-import "github.com/disgoorg/disgo/discord"
+import (
+	"github.com/disgoorg/disgo/discord"
+)
 
 var Commands = []discord.ApplicationCommandCreate{
 	ping,
+	avatar,
 	version,
 }
